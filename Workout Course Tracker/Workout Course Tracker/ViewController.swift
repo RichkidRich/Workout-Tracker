0c6bbc559fb542b351d1deb22c8afb3cd4100970
Richard Lee <richardlee076@gmail.com>
2019-09-20 23:47:07 -0400

    Created Xcode Project

---
+++ Workout Course Tracker/Workout Course Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Workout Course Tracker
+//
+//  Created by Richard Lee on 2019-09-20.
+//  Copyright © 2019 Richard Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
